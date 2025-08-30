a5ee963f4d537f6a9e528c5dd70b7dd5e5f91442
John Grishin <r@exah.me>
2025-08-30 12:57:40 +0100

    ✨ Add download JSON button

--- src/components/stopwatch/stopwatch.tsx
+++ src/components/stopwatch/stopwatch.tsx
@@ -1,19 +1,19 @@
 import { sprinkles } from '../../theme'
 import { Time } from '../../constants'
-import { ProgressCircle } from '../progress-circle'
+import { Button } from '../button'
 import { HStack } from '../h-stack'
+import { LapList } from '../lap-list'
+import { ProgressCircle } from '../progress-circle'
 import { Surface } from '../surface'
 import { Timer } from '../timer'
 import { VStack } from '../v-stack'
 import { ZStack } from '../z-stack'
-import { LapList } from '../lap-list'
 import { StopwatchActions } from './stopwatch-actions'
 import { useStopwatch } from './use-stopwatch'
 
 export function Stopwatch() {
   const stopwatch = useStopwatch()
   const progress = (stopwatch.time % Time.MINUTE) / Time.MINUTE
-
 
   return (
     <Surface>
@@ -43,6 +43,25 @@ export function Stopwatch() {
             <LapList.Item key={lapIndex} lap={lapIndex + 1} time={lapTime} />
           ))}
         </LapList.Root>
+        {stopwatch.laps.length > 0 && (
+          <Button
+            variant="secondary"
+            onClick={() => {
+              const json = JSON.stringify(
+                { time: stopwatch.time, laps: stopwatch.laps },
+                null,
+                2,
+              )
+              const blob = new Blob([json], { type: 'application/json' })
+
+              const objectURL = URL.createObjectURL(blob)
+              window.open(objectURL, '_blank')
+              URL.revokeObjectURL(objectURL)
+            }}
+          >
+            Download JSON
+          </Button>
+        )}
       </VStack>
     </Surface>
   )
